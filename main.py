41e4c8d6fdec9f23c662ee505ff79f692a550fe4
CristiMCV91 <cristi.macovei91@gmail.com>
2024-05-15 20:47:27 +0300

    First commit - empty project

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter
+import customtkinter  # Custom module for GUI
+
+# System Settings
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
+
+# App windows
+app = customtkinter.CTk()
+# app.geometry("1200x720")
+app.title("Typing Speed Test")
+
+
+
+# Run app
+app.mainloop()
